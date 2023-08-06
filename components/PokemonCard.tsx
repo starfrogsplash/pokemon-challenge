9adff354dfe1bbb7d5c22fac7d2c601556b6437e
starfrogsplash <a.khal786@outlook.com>
2023-08-07 00:48:27 +0100

    step2: ui display pokemon

---
+++ components/PokemonCard.tsx
@@ -0,0 +1,37 @@
+import { Pokemon } from '../utils/interfaces';
+import Image from 'next/image';
+
+const PokemonCard = ({ pokemon }: { pokemon: Pokemon }) => {
+  return (
+    <div className="border p-4 rounded-md shadow-md bg-gray-500 flex flex-col items-center justify-between">
+      <div className="mb-4 flex flex-col items-center">
+        <h3 className="text-lg font-semibold text-white mb-2">#{pokemon.id}</h3>
+        <h3 className="text-2xl font-semibold text-white capitalize">{pokemon.name}</h3>
+      </div>
+      <div className="my-2 flex flex-col">
+      <div className="my-4" style={{ width: '100%', height: 'auto'  }}>
+        <Image src={`/sprites/${pokemon.id}.svg`} alt={pokemon.name} width={140} height={140} objectFit="contain" />
+      </div>
+      </div>
+      <div className="text-white text-center">
+        {pokemon.stats.map((stat, i) => (
+          <p key={i} className="capitalize">
+            {stat.stat.name}: {stat.base_stat}
+          </p>
+        ))}
+        <div >
+          <p className="font-semibold mb-2">Types:</p>
+          <div className="flex flex-wrap justify-center">
+            {pokemon.types.map((type, i) => (
+              <span key={i} className="bg-gray-800 px-2 py-1 rounded text-white capitalize mr-2 mb-2">
+                {type.type.name}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,40 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react';
+import { Pokemon } from '../utils/interfaces';
+import PokemonCard from '../components/PokemonCard';
+import axios from 'axios';
 
-const inter = Inter({ subsets: ['latin'] })
+const PokemonPage: React.FC = () => {
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
 
-export default function Home() {
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  const fetchPokemon = async () => {
+    try {
+      const pokemonData = await axios.get('/api/app');
+      setPokemonList(pokemonData.data.pokemonList);
+    } catch (error) {
+      console.error('Error fetching Pokemon data:', error);
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-     hello World
-    </main>
-  )
-}
+    <div className="min-h-screen bg-gray-600 p-4 ">
+      <div className="container mx-auto">
+        <div className="text-4xl font-bold text-white mb-4 text-center">Pokemon</div>
+        {pokemonList.length === 0 ? (
+          <p className="text-white text-center">Loading...</p>
+        ) : (
+          <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {pokemonList.map((pokemon) => (
+              <PokemonCard key={pokemon.id} pokemon={pokemon} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PokemonPage;
